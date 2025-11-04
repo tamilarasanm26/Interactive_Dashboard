3449db8c8decaac592e5e81dcc6c3af025abe8ea
Tamilarasan Murugan <tamilmurugan2602@gmail.com>
2025-11-04 19:11:41 +0530

    added to toast notification

--- activity.js
+++ activity.js
@@ -1,7 +1,7 @@
 const activityLog = [
   { id: 1, name: 'Tamilarasan', action: 'Generated PDF report', time: '09:30 AM' },
   { id: 2, name: 'Sowmiya', action: 'Logged in', time: '09:45 AM' },
-  { id: 3, name: 'Nitesh', action: 'Added a new sale', time: '10:15 AM' }
+  { id: 3, name: 'Nitesh', action: 'Added a new sale', time: '10:15 AM'}
 ];
 
 function renderActivityTable() {
